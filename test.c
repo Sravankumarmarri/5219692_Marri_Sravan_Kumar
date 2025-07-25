ed409571603ef1e7af43f340daea632f924237eb
unknown <sravankumarmarri4@gmail.com>
2025-07-25 13:19:42 +0530

    changing in main

--- test.c
+++ test.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main() {
     int a, b, sum = 0;
-    int c;
+    int c, d;
     printf("Enter two integers: ");
-    scanf("%d %d %d", &a, &b, &c);
-    sum = a + b + c;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    sum = a + b + c + d;
 
     printf("Sum: %d", sum);
 
